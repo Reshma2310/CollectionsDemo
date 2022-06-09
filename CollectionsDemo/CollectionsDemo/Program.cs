89775de750da8f441f2f863f61f2acff70dedb22
Reshma2310 <dreshma253@gmail.com>
2022-06-09 12:41:48 +0500

    [Reshma] - Executing Collection Demo Program

---
+++ CollectionsDemo/CollectionsDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace CollectionsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CollectionsDemo.CollectionClass collection = new CollectionsDemo.CollectionClass();
+            collection.doListDemo();
+            collection.doStackDemo();
+            collection.doQueueDemo();
+            collection.doSetDemo();
+            collection.doDictionaryDemo();
+        }        
+    }
+}
+//FindAll(x => x.state.Equals(cityName))-UC8-UserRegistration, UC9-&&, UC10-Count
